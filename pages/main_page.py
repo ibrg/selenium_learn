4a4fa098230475085658deff02ac6fdd80f6417c
ibrg <romanbezh@gmail.com>
2022-04-11 11:31:39 +0300

    test check for login link

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,8 +1,25 @@
 from.base_page import BasePage
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 
 class MainPage(BasePage):
 
+    def is_element_present(self, how, what):
+        """
+        :param how: ID, CLASS, CSS_SELECTOR
+        :param what: Name id, class or css selector
+        :return:
+        """
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
+
     def go_to_login_page(self):
-        self.browser.find_element(By.ID, 'login_link').click()
+        get_link = self.browser.find_element(By.ID, 'login_link')
+        get_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(By.ID, "login_link"), "Login link is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,18 @@
 from pages.main_page import MainPage
 
 
-def test_guest_can_go_to_login_page(browser):
+def open_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
+    return page
+
+
+def test_guest_can_go_to_login_page(browser):
+    page = open_page(browser)
     page.go_to_login_page()
+
+
+def test_guest_should_see_login_link(browser):
+    page = open_page(browser)
+    page.should_be_login_link()
